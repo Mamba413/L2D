71b4596bce3a3f6276390ccf3da96d37acebf9ac
Jin Zhu <zhuj37@mail2.sysu.edu.cn>
2026-02-14 15:49:44 +0000

    Update rewrite_machine.py
    add Qwen base rewriter

--- scripts/rewrite_machine.py
+++ scripts/rewrite_machine.py
@@ -3,33 +3,36 @@ os.environ["TORCH_COMPILE_DISABLE"] = "1"
 
 import torch
 torch._dynamo.config.disable = True
-import custom_datasets
 from model import load_tokenizer, load_model
 
 PROMPT1 = "You are a rewriting expert and you would rewrite the text without missing the original details. Return ONLY the rewritten version. Do not explain changes, do not give multiple options, and do not add commentary. \n\n Original text: \"{}\" Here is the rewritten version: \n\n"
 
 PROMPT2 = "Revise the following text: \"{}\""
 
+PROMPT3 = "You are a English rewriting expert and you would rewrite the text without missing the original details. Return ONLY the rewritten version. Do not explain changes, do not give multiple options, and do not add commentary. \n\n Original text: \"{}\" Here is the rewritten version: \n\n"
+
 class PrefixSampler:
     def __init__(self, args, rewrite_prompt='l2d'):
         self.args = args
+        self.rewrite_model = args.rewrite_model
         self.base_tokenizer = load_tokenizer(args.rewrite_model, args.cache_dir)
         self.base_model = load_model(args.rewrite_model, args.device, args.cache_dir)
         if rewrite_prompt == 'l2d':
-            self.prompt = PROMPT1
+            if "gemma" in args.rewrite_model:
+                self.prompt = PROMPT1
+            elif "qwen" in args.rewrite_model:
+                self.prompt = PROMPT3
         elif rewrite_prompt == 'bartscore':
             self.prompt = PROMPT2
-
         # self.pipe = pipeline("text-generation", model=self.base_model, tokenizer=self.base_tokenizer, device=torch.cuda.current_device())
 
     def _sample_rewrite_text_from_model(self, texts):
         texts_num_tokens = self.base_tokenizer(texts, return_tensors="pt", padding=True, return_token_type_ids=False)['input_ids'].shape[1]
-        prompt_texts = [self.prompt.format(o) for o in texts] 
 
         self.base_model.eval()
         decoded = ['' for _ in range(len(texts))]
 
-        sampling_kwargs = {'temperature': self.args.temperature}
+        sampling_kwargs = {'temperature': self.args.temperature, "do_sample": True}
         if self.args.do_top_p:
             sampling_kwargs['top_p'] = self.args.top_p
         elif self.args.do_top_k:
@@ -40,9 +43,19 @@ class PrefixSampler:
         sampling_kwargs["eos_token_id"] = self.base_tokenizer.eos_token_id
         sampling_kwargs['pad_token_id'] = self.base_tokenizer.eos_token_id
 
+        if "gemma" in self.args.rewrite_model:
+            prompt_texts = [self.prompt.format(o) for o in texts] 
+        elif "qwen" in self.args.rewrite_model:
+            prompt_texts = self.format(texts[0])
+            prompt_texts = self.base_tokenizer.apply_chat_template(
+                [{"role": "user", "content": prompt_texts}],
+                tokenize=False,
+                add_generation_prompt=True,
+                enable_thinking=False # Turn of thinking modes
+            )
         all_encoded = self.base_tokenizer(prompt_texts, return_tensors="pt", padding=True, return_token_type_ids=False).to(self.args.device)
         prompt_lens = all_encoded['input_ids'].shape[1]
-        outputs = self.base_model.generate(**all_encoded, do_sample=True, **sampling_kwargs)
+        outputs = self.base_model.generate(**all_encoded, **sampling_kwargs)
         gen_ids = outputs[:, prompt_lens:]
         decoded = self.base_tokenizer.batch_decode(gen_ids, skip_special_tokens=True)
 
